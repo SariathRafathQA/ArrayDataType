48d75c99a9fb9df2daa0fd372b8b45034e2da9a6
SariathRafathQa <sariathrafathqa@gmail.com>
2021-01-16 18:37:28 -0500

    ForLoop

---
+++ src/com/company/ForLoopDataType.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ForLoopDataType {
+    public static void main (String[]args) {
+
+            int n = 6;
+            for (int i = 1; i < n; i++) {
+                System.out.println("I like Java");
+            }
+        }
+    }
