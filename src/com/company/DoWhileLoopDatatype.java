93dcb5238b139f1015bd0956122b5887c0040214
SariathRafathQa <sariathrafathqa@gmail.com>
2021-01-16 19:03:45 -0500

    WhileLoopDataType

---
+++ src/com/company/DoWhileLoopDatatype.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class DoWhileLoopDatatype {
+    public static void main(String[]args){
+        int x=10;
+        do{
+            System.out.println(x);
+            x++;
+        } while (x<=20);
+    }
+}
