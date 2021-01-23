49326055fce8186083fd65f4b6f3537aaa72e340
SariathRafathQa <sariathrafathqa@gmail.com>
2021-01-23 18:08:15 -0500

    IntSameVariableDifferentValues

---
+++ src/FirstPratice/IntSameVariableDifferentValues.java
@@ -0,0 +1,11 @@
+package FirstPratice;
+
+public class IntSameVariableDifferentValues {
+        public static void main(String[] args) {
+            int myNum = 10;
+            myNum = 15;
+            myNum= 20; // I practiced keep same variable but different values.
+            System.out.println(myNum);
+        }
+    }
+
