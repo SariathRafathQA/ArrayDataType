dc7be54e7cefa0d1375ce49ddc7e21a166f22535
SariathRafathQa <sariathrafathqa@gmail.com>
2021-02-20 16:07:19 -0500

    Merge remote-tracking branch 'github/main'

---
+++ src/FirstPratice/ProgrammerCc.java
@@ -0,0 +1,12 @@
+package FirstPratice;
+
+class ProgrammerCc extends EmployeePc {
+        int bonus=10000;
+        public static void main(String[] args){
+            ProgrammerCc p=new ProgrammerCc();
+            System.out.println("Programmer salary is:"+p.salary);
+
+            //System.out.println("Bonus of Programmer is:"+p.bonus);
+        }
+    }
+
