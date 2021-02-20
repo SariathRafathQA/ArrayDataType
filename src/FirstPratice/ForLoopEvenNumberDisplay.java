96de395739b673ce66892d836ddc61816a8b6e20
SariathRafathQa <sariathrafathqa@gmail.com>
2021-02-20 16:13:24 -0500

    Merge remote-tracking branch 'github/main'

---
+++ src/FirstPratice/ForLoopEvenNumberDisplay.java
@@ -0,0 +1,20 @@
+package FirstPratice;
+
+public class ForLoopEvenNumberDisplay {
+    public static void main(String[] args){
+
+        int number=100;
+
+        System.out.println("List of even numbers from 1 to "+number+": ");
+        for (int i=1; i<=number; i++)
+        {
+//logic to check if the number is even or equal
+//if i%2 is equal to zero, the number is even
+            if (i%2==0)
+            {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
+
